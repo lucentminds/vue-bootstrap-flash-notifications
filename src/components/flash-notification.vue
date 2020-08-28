8947d504bce5095540b6f693ad840884a94a4424
lucentminds <scott@lifecorp.com>
2020-08-27 20:25:55 -0500

    Remove animation add close icon.

--- src/components/flash-notification.vue
+++ src/components/flash-notification.vue
@@ -1,7 +1,12 @@
 <template>
    <div class="notification" ref="notification">
-      <div :class="`flash-notification alert alert-${notification.type}`">
-         {{notification.message}}
+      <div :class="`notification__alert alert alert-${notification.type}`">
+         <div class="notification__text">
+            {{notification.message}}
+         </div>
+         <div class="notification__icon-close" title="Close this notification" @click="close_notification()">
+            <i>&times;</i>
+         </div>
       </div>
    </div>
 </template>
@@ -11,47 +16,87 @@
 export default {
    props:{
       notification: Object,
-   },
+   },// /props{}
 
    data(){
       return {
          timeout: 0
       }
-   },
+   },// /date()
 
    methods: {
+      close_notification(){
+         const note = this.$refs.notification;
+         this.flash_notification_remove( this.notification.id );
+         note.classList.add( '--closed' );
+      },// /close_notification()
 
    },// methods{}
 
    mounted(){
-      const note = this.$refs.notification;
       
-      clearTimeout( this.timeout );
+      // clearTimeout( this.timeout );
 
-      this.timeout = setTimeout(() => {
-         this.flash_notification_remove( this.notification.id );
-         note.classList.add( '--closed' );
-      }, 2000 );
-   },
+      // this.timeout = setTimeout(() => {
+      //    this.flash_notification_remove( this.notification.id );
+      //    note.classList.add( '--closed' );
+      // }, 2000 );
+   },// /mounted()
 
    beforeDestroy(){
       clearTimeout( this.timeout );
    }
-}
+};
 </script>
 
 
 <style scoped>
+/* 
 .notification
 {
    max-height: 1024px;
+   opacity: 1;
    overflow: hidden;
-   transition-property: all;
+   transition-property: opacity;
    transition-duration: 5s;
    transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
-}
+} */
+
 .notification.--closed
 {
-   max-height: 0;
+   /* opacity: 0; */
+   display: none;
+}
+
+.notification__alert
+{
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   width: 100%;
+   padding: 0;
+   /* padding: 1em; */
+}
+
+.notification__text
+{
+   padding: 0.5em 1em 0.5em 1em;
+}
+
+.notification__icon-close
+{
+   padding: 0.5em 1em 0.5em 1em;
+   cursor: pointer;
+}
+
+.notification__icon-close i
+{
+   font-weight: bold;
+   font-size: 1.5em;
+}
+
+.notification__icon-close:hover
+{
+   background-color: rgba( 0, 0, 0, 0.25 );
 }
 </style>
